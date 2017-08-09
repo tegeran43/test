a2af98b61b8efc1ac1fbaab32ee165c6381f7703
Ilya <vik.kor.1917@mail.ru>
2017-08-09 16:07:53 +0300

    initial commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static string Out()
+        {
+            return "hello";
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(Out());
+
+        }
+
+        
+    }
+     
+}
